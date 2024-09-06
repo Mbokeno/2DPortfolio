ba407d0f95dd0ed27198529da9a01760735ae75c
Mbokeno <132112593+Mbokeno@users.noreply.github.com>
2024-09-05 21:14:16 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/2Dportfolio/',
+  base: '/2DPortfolio/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
