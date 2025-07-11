20455c1886f8e6df6848561cbc283eeeed510750
Mbokeno <132112593+Mbokeno@users.noreply.github.com>
2025-07-11 00:10:19 -0400

    added in new resume

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -116,7 +116,7 @@ const Hero = () => {
                         <HorizontalLine variants={itemVariants}/>
                         <ContactSection>
                            <motion.h4 variants={itemVariants}>Email: bryanbokino@outlook.com</motion.h4>
-                           <ResumeLink variants={itemVariants} href="/2DPortfolio/Resume.pdf" target="_blank" rel="noopener noreferrer"><AdaptiveBackgroundText> My Resume </AdaptiveBackgroundText></ResumeLink>
+                           <ResumeLink variants={itemVariants} href="/2DPortfolio/Resume2.pdf" target="_blank" rel="noopener noreferrer"><AdaptiveBackgroundText> My Resume </AdaptiveBackgroundText></ResumeLink>
                            <ResumeLink variants={itemVariants} href="https://github.com/Mbokeno" target="_blank" rel="noopener noreferrer"><AdaptiveBackgroundText> My Github </AdaptiveBackgroundText></ResumeLink>
                         </ContactSection>
                     </Information>
